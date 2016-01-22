eeeb0657063f340c192e051ea926ad9d8d6600e8
Hugo <hugo.leray.scheidt@gmail.com>
2016-01-22 15:06:35 +0100

    Fix a tester

---
+++ Assets/scripts/item/CanonBall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * @author Adrien D
+ * @version 1.0
+ */
+
+/**
+ * Potion script
+ */
+
+public class CanonBall : MonoBehaviour {
+
+
+	public CanonBall(){
+		Debug.Log ("Position:"+this.gameObject.transform.position.y);
+	}
+}
