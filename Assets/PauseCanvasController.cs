792c27d3b94496547b61fcca4eb27a8ce2b1c184
Baptiste <baptiste.valthier@gmail.com>
2016-01-29 12:28:48 +0100

    Add menu opening sound -TODO: menu closing sound.

---
+++ Assets/PauseCanvasController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseCanvasController : MonoBehaviour {
+
+	AudioSource audioSource;
+	AudioClip soundClosing;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		soundClosing = Resources.Load("sounds/pop-up-close") as AudioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDisable() {
+		print("script was removed");
+		audioSource.PlayOneShot(soundClosing);
+	}
+}
